8adafb12a35049f45819442679d867ccaf7469ea
jonssonyan <yz808@outlook.com>
2024-02-05 16:10:49 +0800

    fix: util

--- util/arr.go
+++ util/arr.go
@@ -1,31 +1,24 @@
 package util
 
-func SplitArr[T any](arr []T, num int64) [][]T {
-	max := int64(len(arr))
-	//判断数组大小是否小于等于指定分割大小的值，是则把原数组放入二维数组返回
-	if max <= num {
+func SplitArr[T any](arr []T, num int) [][]T {
+	length := len(arr)
+	if length <= num {
 		return [][]T{arr}
 	}
-	//获取应该数组分割为多少份
-	var quantity int64
-	if max%num == 0 {
-		quantity = max / num
-	} else {
-		quantity = (max / num) + 1
-	}
-	//声明分割好的二维数组
-	var segments = make([][]T, 0)
-	//声明分割数组的截止下标
-	var start, end, i int64
-	for i = 1; i <= quantity; i++ {
-		end = i * num
-		if i != quantity {
-			segments = append(segments, arr[start:end])
-		} else {
-			segments = append(segments, arr[start:])
+
+	quantity := (length + num - 1) / num
+	segments := make([][]T, 0, quantity)
+
+	for i := 0; i < quantity; i++ {
+		end := (i + 1) * num
+		if end > length {
+			end = length
 		}
-		start = i * num
+
+		segment := arr[i*num : end]
+		segments = append(segments, segment)
 	}
+
 	return segments
 }
 
